3b3ac7fc6a18046d36567969ffab5524946b1e3b
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-07-17 22:27:13 +0200

    Add docs needed for sphinx auto generation

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,36 @@
-# conf.py
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-project = 'Your Project Name'
-author = 'Your Name'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Finance'
+copyright = '2023, Amir Nazary'
+author = 'Amir Nazary'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
-# The master toctree document.
-master_doc = 'index'
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# General information about the project.
-project = 'Your Project Name'
-copyright = '2023, Your Name'
-author = 'Your Name'
 
-# The version info
-release = '1.0.0'
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
