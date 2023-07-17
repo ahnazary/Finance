a61b17db81cf19acbceb8ddc479c6abe6facf350
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-07-17 22:09:29 +0200

    Create sphinx workflow and github action

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# conf.py
+
+project = 'Your Project Name'
+author = 'Your Name'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = 'Your Project Name'
+copyright = '2023, Your Name'
+author = 'Your Name'
+
+# The version info
+release = '1.0.0'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
