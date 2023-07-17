7528c4d17045d6fcaf0ce7422fc71bfe53aea447
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-07-17 22:28:12 +0200

    Fix black

--- docs/conf.py
+++ docs/conf.py
@@ -8,29 +8,25 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Finance'
-copyright = '2023, Amir Nazary'
-author = 'Amir Nazary'
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "Finance"
+copyright = "2023, Amir Nazary"
+author = "Amir Nazary"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
