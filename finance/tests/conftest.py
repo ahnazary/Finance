a6b31e315de1381d1ae82caab8288ed27c97ee6f
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-08-18 23:39:22 +0200

    Add fixtures to setup dbs

---
+++ finance/tests/conftest.py
@@ -0,0 +1,21 @@
+from pytest import fixture
+from sqlalchemy import create_engine
+
+@fixture
+def setup_tables(test_engine):
+    """
+    Fixture to set up the tables in the database
+    """
+    # execute schema.sql
+    with open("finance/tests/schemas.sql", "r") as f:
+        query = f.read()
+    with test_engine.connect() as conn:
+        conn.execute(query)
+    
+@fixture
+def test_engine():
+    """
+    Test to check if the engine is created correctly
+    """
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+    return engine
--- finance/tests/test_utils.py
+++ finance/tests/test_utils.py
@@ -1,5 +1,28 @@
 import pytest
+from sqlalchemy import MetaData, Table, select
 
+def test_tables_exist(setup_tables, test_engine):
+    """
+    Test to check if the tables are created correctly
+    """
+    # List of tables in local postgres database in stocks schema
+    metadata = MetaData()
+    information_schema_tables = Table(
+        "tables", metadata, autoload_with=test_engine, schema="information_schema"
+    )
+    query = (
+        select(information_schema_tables.c.table_name)
+        .where(information_schema_tables.c.table_schema == "stocks")
+        .order_by(information_schema_tables.c.table_name)
+    )
 
-def test_dummy():
-    assert True
+    with test_engine.connect() as conn:
+        tables = [table[0] for table in conn.execute(query).fetchall()]
+    assert tables == [
+        "financials",
+        "income_statement",
+        "balance_sheet",
+        "cash_flow",
+        "tickers_list",
+        "valid_tickers",
+    ]
