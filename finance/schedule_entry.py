86738a110b31074aa0f227e16b098a84c6908fff
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-08-24 17:30:35 +0200

    Fix batch and schedule

--- finance/schedule_entry.py
+++ finance/schedule_entry.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 provider = os.environ.get("PROVIDER")
 
-schedule_jobs = ScheduleJobs(provider=provider)
+schedule_jobs = ScheduleJobs(provider=provider, batch_size=300)
 schedule_jobs.update_table_batch(table_name="cash_flow", engine=schedule_jobs.engine)
