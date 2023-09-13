d74715e38e5c08620eed7ebda8d2f54776d87338
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-09-13 23:54:37 +0200

    Test none records error fix

--- finance/schedule_jobs/annual_income_sttm.py
+++ finance/schedule_jobs/annual_income_sttm.py
@@ -21,7 +21,6 @@ schedule_jobs = ScheduleJobs(provider=provider, batch_size=config.BATCH_SIZE)
 tickers_list = schedule_jobs.get_tickers_batch_backfill(
     table_name=table_name, engine=schedule_jobs.engine, frequency="annual"
 )
-tickers_list = [x for x in tickers_list if x is not None]
 
 # getting a list[yf.Ticker] of old tickers with batch_size
 tickers_yf_batch = schedule_jobs.get_tickers_batch_yf_object(tickers_list=tickers_list)
@@ -37,9 +36,8 @@ for ticker_yf_obj in tickers_yf_batch:
         table_name=table_name,
         table_columns=table_columns,
     )
-    if not record:
-        continue
-    records.append(record)
+    if record is not None:
+        records.append(record)
 
 
 # convert list[list[dict]] to list[dict]
