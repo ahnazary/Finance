84f743bb370facb2255a54fb96550729caab5fed
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-10-28 23:29:17 +0200

    Add comment of config files

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+# Description: Configuration file for the application
+
+# Number of tickers to be processed in a batch for jobs that fill the database
 BATCH_SIZE = 300
+
+# Only stocks that have earnings released in the following currencies will be processed
 CURRENCIES = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CHF"]
+
 # warning log level
 LOG_LEVEL = 30
