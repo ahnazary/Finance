5c6118f66a230ed097cbbca9ebd9a117064f62c1
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-11-13 18:08:30 +0100

    Set config to 50

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Description: Configuration file for the application
 
 # Number of tickers to be processed in a batch for jobs that fill the database
-BATCH_SIZE = 200
+BATCH_SIZE = 50
 
 # Only stocks that have earnings released in the following currencies will be processed
 CURRENCIES = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CHF"]
