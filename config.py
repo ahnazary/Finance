43a1cfdffdd84c9da82ba8931dce725086a392f4
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-11-21 21:39:06 +0100

    Set config to 800

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Description: Configuration file for the application
 
 # Number of tickers to be processed in a batch for jobs that fill the database
-BATCH_SIZE = 500
+BATCH_SIZE = 800
 
-# Only stocks that have earnings released in the following currencies will be processed
+# Only stocks that have earnings released in the following currencies will be processed 
 CURRENCIES = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CHF"]
 
 # warning log level
