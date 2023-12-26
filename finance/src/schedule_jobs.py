5b63686d4b48dab84cc574bc5e29ad4e0a9e0167
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-12-26 06:18:12 +0100

    Fix import

--- finance/src/schedule_jobs.py
+++ finance/src/schedule_jobs.py
@@ -15,7 +15,7 @@ sys.path.insert(
 import sqlalchemy
 import yfinance as yf
 from sqlalchemy import asc, distinct, func, select
-from src.extract import Ticker
+from finance.src.yahoo_ticker import Ticker
 from src.postgres_interface import PostgresInterface
 from src.utils import custom_logger
 
