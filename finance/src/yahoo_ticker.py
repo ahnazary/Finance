fc494fbc9713798551a186844014220c73699f1c
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-12-26 07:31:19 +0100

    Fix flake8

--- finance/src/yahoo_ticker.py
+++ finance/src/yahoo_ticker.py
@@ -51,7 +51,8 @@ class Ticker:
     def update_tickers_list_table(self):
         """
         Method to update the tickers_list table in postgres
-        Gets all the data in the data dir excel file and inserts it into the database
+        Gets all the data in the data dir excel file (all available tickers) and
+        inserts them into the database
         """
         valids_df = pd.read_excel("src/data/tickers_list.xlsx")
         # rename columns to match the database
@@ -76,123 +77,6 @@ class Ticker:
             method="multi",
             chunksize=1000,
         )
-
-    def update_valid_tickers_table(self):
-        """
-        Method to update the valid_tickers table in postgres
-        Gets all the tickers from the tickers_list table and checks if they are valid
-
-        validity check:
-            - ticker must have market cap
-            - ticker must have total revenue
-            - ticker must have free cash flow
-            - ticker must have total assets
-            - ticker must have currency code
-        """
-
-        # query all the tickers from the tickers_list table that are not in valid_tickers table
-        tickers_list = Table(
-            "tickers_list", MetaData(), autoload_with=self.engine, schema="stocks"
-        )
-        valid_tickers = Table(
-            "valid_tickers",
-            MetaData(),
-            autoload_with=self.engine,
-            schema="stocks",
-        )
-
-        query = (
-            select(tickers_list.c.ticker)
-            .outerjoin(valid_tickers, tickers_list.c.ticker == valid_tickers.c.ticker)
-            .where(valid_tickers.c.ticker == null())
-        )
-
-        with self.engine.connect() as conn:
-            tickers = [result[0] for result in conn.execute(query).fetchall()]
-
-        valids_df = pd.DataFrame(
-            columns=[
-                "ticker",
-                "currency_code",
-                "market_cap",
-                "total_revenue",
-                "free_cash_flow",
-                "total_assets",
-                "validity",
-            ]
-        )
-        invalids_df = pd.DataFrame(columns=["ticker", "validity"])
-
-        for ticker_symbol in tickers:
-            ticker = yf.Ticker(ticker_symbol)
-
-            if len(valids_df) > self.chunksize:
-                # insert the data into the database
-                valids_df.to_sql(
-                    name="valid_tickers",
-                    con=self.engine,
-                    if_exists="append",
-                    schema="stocks",
-                    index=False,
-                    method="multi",
-                )
-                # empty the valids_df
-                valids_df = valids_df.iloc[0:0]
-
-                self.logger.warning(
-                    f"Inserted {self.chunksize} rows for valid tickers to valid_tickers table"
-                )
-
-            if len(invalids_df) > self.chunksize:
-                # insert the data into the database
-                invalids_df.to_sql(
-                    name="valid_tickers",
-                    con=self.engine,
-                    if_exists="append",
-                    schema="stocks",
-                    index=False,
-                    method="multi",
-                )
-                # empty the invalids_df
-                invalids_df = invalids_df.iloc[0:0]
-
-                self.logger.warning(
-                    f"Inserted {self.chunksize} rows for invalid tickers"
-                )
-
-            # check if the ticker is valid
-            try:
-                if (
-                    ticker.info["marketCap"]
-                    and ticker.financials.loc["Total Revenue"].values[0]
-                    and ticker.cashflow.loc["Free Cash Flow"].values[0]
-                    and ticker.balance_sheet.loc["Total Assets"].values[0]
-                    and ticker.info["currency"]
-                ):
-                    valids_df.loc[len(valids_df)] = pd.Series(
-                        {
-                            "ticker": ticker_symbol,
-                            "currency_code": ticker.info["currency"],
-                            "market_cap": ticker.info["marketCap"],
-                            "total_revenue": ticker.financials.loc[
-                                "Total Revenue"
-                            ].values[0],
-                            "free_cash_flow": ticker.cashflow.loc[
-                                "Free Cash Flow"
-                            ].values[0],
-                            "total_assets": ticker.balance_sheet.loc[
-                                "Total Assets"
-                            ].values[0],
-                            "validity": True,
-                        }
-                    )
-
-            except Exception as e:
-                self.logger.warning(f"Error checking validity of {ticker_symbol}: {e}")
-                self.logger.warning(f"Ticker {ticker_symbol} is invalid")
-                invalids_df.loc[len(invalids_df)] = pd.Series(
-                    {"ticker": ticker_symbol, "validity": False}
-                )
 
     def load_valid_tickers(self, sink_table: str) -> List[str]:
         """
