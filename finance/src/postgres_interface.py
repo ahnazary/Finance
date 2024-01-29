5ff9cabd5d037ddd41859a9e4d68b466a9ccff62
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-01-30 00:44:49 +0100

    Update migrate script

--- finance/src/postgres_interface.py
+++ finance/src/postgres_interface.py
@@ -10,9 +10,9 @@ import os
 import pandas as pd
 import sqlalchemy
 from dotenv import load_dotenv
-from sqlalchemy import MetaData, Table, select
+from sqlalchemy import MetaData, Table, select, text
 from sqlalchemy.dialects.postgresql import insert
-from src.utils import custom_logger
+from utils import custom_logger
 
 
 class PostgresInterface:
@@ -39,8 +39,7 @@ class PostgresInterface:
         host = os.environ.get(f"{self.provider}_POSTGRES_HOST")
         port = os.environ.get(f"{self.provider}_POSTGRES_PORT")
         db = os.environ.get(f"{self.provider}_POSTGRES_DB")
-
-        ssl_mode = "" if self.provider == "LOCAL" else "?sslmode=require"
+        ssl_mode = "?sslmode=" + os.environ.get(f"{self.provider}_SSL_MODE") or ""
 
         engine = sqlalchemy.create_engine(
             f"postgresql://{user}:{password}@{host}:{port}/{db}{ssl_mode}"
@@ -72,57 +71,6 @@ class PostgresInterface:
         metadata = MetaData()
         table = Table(table_name, metadata, autoload_with=engine, schema=schema)
         return table
-
-    def migrate_local_to_cloud(self) -> None:
-        """
-        Method to migrate the local database to the neon database
-        Supposed to be used only once to migrate the data from the local
-        to a target neon database (for now NEON cloud database)
-
-        Returns
-        -------
-        None
-        """
-        engines = self.create_engine()
-        engine_local = engines["local"]
-        engine_neon = engines["neon"]
-
-        # List of tables in local postgres database in stocks schema
-        metadata = MetaData()
-        information_schema_tables = Table(
-            "tables", metadata, autoload_with=engine_local, schema="information_schema"
-        )
-        query = (
-            select(information_schema_tables.c.table_name)
-            .where(information_schema_tables.c.table_schema == "stocks")
-            .order_by(information_schema_tables.c.table_name)
-        )
-
-        with engine_local.connect() as conn_local:
-            tables = [table[0] for table in conn_local.execute(query).fetchall()]
-
-        # insert table's data into neon database
-        for table in tables:
-            self.logger.warning(f"Inserting data from {table} into neon database")
-            with engine_local.connect() as conn_local:
-                with engine_neon.connect() as conn_neon:
-                    table_local = self.create_table_object(table, engine_local)
-                    query = select(table_local)
-                    data = [tuple(row) for row in conn_local.execute(query).fetchall()]
-
-                    # cast data into batches of 1000 rows
-                    data = [
-                        data[i : i + 1000]  # noqa: E203
-                        for i in range(0, len(data), 1000)
-                    ]
-
-                    table_neon = self.create_table_object(table, engine_neon)
-                    inserted_batches = 0
-                    for batch in data:
-                        # statement to insert data into neon database
-                        self.logger.warning(f"Inserting batch of {len(batch)} rows")
-                        self.insert_batch(table=table_neon, batch=batch, conn=conn_neon)
-                        self.logger.warning(f"Inserted {inserted_batches} batches")
 
     def insert_batch(
         self, table: sqlalchemy.Table, batch: list, conn: sqlalchemy.engine.Connection
@@ -168,3 +116,106 @@ class PostgresInterface:
             result = conn.execute(query).fetchall()
         df = pd.DataFrame(result, columns=table.columns.keys())
         return df
+
+    def migrate_dbs(self, batch_size: int = 5000) -> None:
+        """
+        Method to migrate a database to another one
+        Supposed to be used only once to migrate data
+        to a target database
+
+        Parameters
+        ----------
+        batch_size : int
+            number of rows to insert in each batch
+            default: 5000
+
+        Returns
+        -------
+        None
+        """
+        tap_cloud_provider = "NEON"
+        tap_user = os.environ.get(f"{tap_cloud_provider}_POSTGRES_USER")
+        tap_password = os.environ.get(f"{tap_cloud_provider}_POSTGRES_PASSWORD")
+        tap_host = os.environ.get(f"{tap_cloud_provider}_POSTGRES_HOST")
+        tap_port = os.environ.get(f"{tap_cloud_provider}_POSTGRES_PORT")
+        tap_db = os.environ.get(f"{tap_cloud_provider}_POSTGRES_DB")
+        engine_tap = sqlalchemy.create_engine(
+            f"postgresql://{tap_user}:{tap_password}@{tap_host}:{tap_port}/{tap_db}"
+        )
+
+        target_cloud_provider = "AVN"
+        target_user = os.environ.get(f"{target_cloud_provider}_POSTGRES_USER")
+        target_password = os.environ.get(f"{target_cloud_provider}_POSTGRES_PASSWORD")
+        target_host = os.environ.get(f"{target_cloud_provider}_POSTGRES_HOST")
+        target_port = os.environ.get(f"{target_cloud_provider}_POSTGRES_PORT")
+        target_db = os.environ.get(f"{target_cloud_provider}_POSTGRES_DB")
+        engine_target = sqlalchemy.create_engine(
+            f"postgresql://{target_user}:{target_password}@{target_host}:{target_port}/{target_db}"
+        )
+
+        # List of tables in local postgres database in stocks schema
+        metadata = MetaData()
+        information_schema_tables = Table(
+            "tables", metadata, autoload_with=engine_tap, schema="information_schema"
+        )
+        query = (
+            select(information_schema_tables.c.table_name)
+            .where(information_schema_tables.c.table_schema == "stocks")
+            .order_by(information_schema_tables.c.table_name)
+        )
+
+        with engine_tap.connect() as conn_local:
+            tables = [table[0] for table in conn_local.execute(query).fetchall()]
+
+        # if "alembic" of "dbt" are in table's name, remove them
+        blacklist = ["alembic", "dbt"]
+        tables = [table for table in tables if not any(x in table for x in blacklist)]
+
+        # insert table's data into neon database
+        for table in tables:
+            self.logger.warning(f"Inserting data from {table} into target database")
+            with engine_tap.connect() as conn_tap:
+                with engine_target.connect() as conn_target:
+                    table_tap = self.create_table_object(table, engine_tap)
+                    total_rows = conn_tap.execute(
+                        text("SELECT COUNT(*) FROM stocks." + table)
+                    ).scalar()
+                    self.logger.warning(
+                        f"Total rows in {table_tap} from tap database: {total_rows}"
+                    )
+
+                    # Calculate how many iterations you will need
+                    iterations = total_rows // batch_size + (
+                        1 if total_rows % batch_size else 0
+                    )
+
+                    for i in range(iterations):
+                        offset = i * batch_size
+                        # Create a SELECT statement with LIMIT and OFFSET
+                        select_stmt = conn_tap.execute(
+                            text(
+                                "SELECT * FROM stocks."
+                                + table
+                                + f" LIMIT {batch_size} OFFSET {offset}"
+                            )
+                        )
+                        result_set = select_stmt.fetchall()
+                        # convert result set to list of dicts
+                        result_set = [tuple(row) for row in result_set]
+                        self.logger.warning(
+                            f"""Selected {len(result_set)} rows from {table} from tap database
+                             with offset {offset}"""
+                        )
+
+                        table_target = self.create_table_object(table, engine_target)
+
+                        # statement to insert data into neon database
+                        self.logger.warning(
+                            f"Inserting batch of {len(result_set)} rows"
+                        )
+                        self.insert_batch(table_target, result_set, conn_target)
+                        self.logger.warning(
+                            f"Inserted {len(result_set)} rows from {table} into target database"
+                        )
+
+                        del result_set
