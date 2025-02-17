2a6a7d2660a7f6ead8d3e9df7ab3d567caa44993
ahnazary <ah.nazary.aghchemazary@gmail.com>
2025-02-17 06:34:30 +0100

    Fix logger bug

--- finance/schedule_jobs/fill_valid_tickers_table.py
+++ finance/schedule_jobs/fill_valid_tickers_table.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from dotenv import load_dotenv
-from src.utils import custom_logger
+from src.utils import emit_log
 
 import config
 from finance.src.jobs import Jobs
@@ -15,7 +15,6 @@ load_dotenv()
 if __name__ == "__main__":
     provider = os.getenv("PROVIDER")
     table_name = "valid_tickers"
-    logger = custom_logger(logger_name=table_name, log_level=config.LOG_LEVEL)
 
     jobs = Jobs(
         provider=provider,
@@ -23,3 +22,4 @@ if __name__ == "__main__":
     )
 
     jobs.fill_valid_tickers_table()
+    emit_log(f"Successfully filled {table_name} table.")
