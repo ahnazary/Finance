f152e6530571af476699e255eccfe199811dce4c
ahnazary <ah.nazary.aghchemazary@gmail.com>
2025-04-11 21:27:58 +0200

    Load env with doteenv for back up to s3 workflow

--- finance/schedule_jobs/backup_pg_to_s3.py
+++ finance/schedule_jobs/backup_pg_to_s3.py
@@ -5,6 +5,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from src.s3_interface import S3Interface
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     s3_interface = S3Interface()
